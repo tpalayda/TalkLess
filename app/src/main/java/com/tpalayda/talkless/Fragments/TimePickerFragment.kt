bb861df4852d0277f931fa4bc5e283ab51aa000a
tpalayda <taras.palayda@gmail.com>
2019-05-05 15:36:35 +0200

    time picker

---
+++ app/src/main/java/com/tpalayda/talkless/Fragments/TimePickerFragment.kt
@@ -0,0 +1,25 @@
+package com.tpalayda.talkless.Fragments
+
+import android.app.Dialog
+import android.app.DialogFragment
+import android.app.TimePickerDialog
+import android.os.Bundle
+import android.widget.TimePicker
+import android.text.format.DateFormat;
+import java.util.*
+
+class TimePickerFragment : DialogFragment(), TimePickerDialog.OnTimeSetListener {
+
+    override fun onCreateDialog(savedInstanceState: Bundle): Dialog {
+        val c = Calendar.getInstance()
+
+        val hour = c.get(Calendar.HOUR_OF_DAY)
+        val minute = c.get(Calendar.MINUTE)
+
+        return TimePickerDialog(activity, this, hour, minute, DateFormat.is24HourFormat(activity))
+    }
+
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
--- app/src/main/java/com/tpalayda/talkless/MainActivity.kt
+++ app/src/main/java/com/tpalayda/talkless/MainActivity.kt
@@ -11,30 +11,97 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
+import android.widget.TimePicker
 import com.tpalayda.talkless.Settings.SettingsActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
+import java.text.DecimalFormat
+import java.text.NumberFormat
+
+
+//TODO save state when rotate
+//TODO preference fragment (choosing vibration time, color of the background)
+//TODO countdownTimer to another file
+//TODO refresh button/play button/pause button centered below text?
 
 class MainActivity : AppCompatActivity() {
-    //private lateinit var button : Button
     companion object {
         const val MY_PERMISSIONS_VIBRATE = 1
     }
+
+    private var totalTime : Long = 0
+    private var vibrationTime : Long = 0
+    private lateinit var countdownTimer : CountDownTimer
+    private var i = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         checkPermissions()
-        test_button.setOnClickListener {
-            updateProgressBar()
-            /*            val v = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
 
-            if(Build.VERSION.SDK_INT >= 26) {
-                v.vibrate(VibrationEffect.createOneShot(1000, 1))
-            } else {
-                v.vibrate(1000);
-            }*/
+        setNumberPickerFormatters()
+        setNumberPickerValues()
+
+        countdownButton.setOnClickListener {
+            totalTime = 0
+            totalTime += hoursNumberPicker.value * 3600000
+            totalTime += minutesNumberPicker.value * 60000
+            totalTime += secondsNumberPicker.value * 1000
+
+            hideNumberPickers()
+
+            countdownText.visibility = View.VISIBLE
+            pauseButton.visibility = View.VISIBLE
+            countdownButton.visibility = View.GONE
+
+            countdownTimer = object : CountDownTimer(totalTime, 100) {
+                override fun onTick(millisUntilFinished: Long) {
+                    val minutes = millisUntilFinished / 60000
+                    val seconds = millisUntilFinished % 60000 / 1000
+
+                    var timeLeftText = "" + minutes + ":"
+                    if(seconds < 10)
+                        timeLeftText += "0"
+                    timeLeftText += seconds
+                    ++i
+                    countdownText.text = timeLeftText
+
+                    val fraction : Float = millisUntilFinished/totalTime.toFloat()
+                    progressBar.progress = (fraction*100).toInt()
+                }
+
+                override fun onFinish() {
+                    progressBar.progress = 0
+
+                    if(vibrationTime > 0) {
+                        val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+                        if(Build.VERSION.SDK_INT >= 26) {
+                            vibrator.vibrate(VibrationEffect.createOneShot(vibrationTime, 10))
+                        } else {
+                            vibrator.vibrate(vibrationTime);
+                        }
+                    }
+
+                    countdownText.visibility = View.GONE
+                    pauseButton.visibility = View.GONE
+                    countdownButton.visibility = View.VISIBLE
+                    showNumberPickers()
+                }
+            }.start()
+        }
+
+        pauseButton.setOnClickListener {
+            countdownTimer.cancel()
+            pauseButton.visibility = View.GONE
+            continueButton.visibility = View.VISIBLE
+        }
+
+        continueButton.setOnClickListener {
+            countdownTimer.start()
+            pauseButton.visibility = View.VISIBLE
+            continueButton.visibility = View.GONE
         }
     }
 
@@ -42,7 +109,7 @@ class MainActivity : AppCompatActivity() {
         if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.VIBRATE) != PackageManager.PERMISSION_GRANTED) {
             requestPermission()
         } else {
-            Log.wtf("123", "permission granted");
+            Log.wtf("123", "permission granted")
         }
     }
 
@@ -66,18 +133,49 @@ class MainActivity : AppCompatActivity() {
             R.id.settings -> {
                 val intent = Intent(this, SettingsActivity::class.java)
                 startActivity(intent)
-                return true;
+                return true
             }
         }
         return true
     }
-    private fun updateProgressBar() {
-        doAsync {
-            var progress = 0
-            while(progress < 100) {
-                progressBar.progress = ++progress
-                SystemClock.sleep(300)
-            }
+
+    private fun setNumberPickerFormatters() {
+        minutesNumberPicker.setFormatter {
+            it->
+            DecimalFormat("00").format(it)
         }
+        secondsNumberPicker.setFormatter {
+            it ->
+            DecimalFormat("00").format(it)
+        }
+        hoursNumberPicker.setFormatter {
+            it ->
+            DecimalFormat("00").format(it)
+        }
+    }
+
+    private fun setNumberPickerValues() {
+        hoursNumberPicker.maxValue = 23
+        hoursNumberPicker.minValue = 0
+
+        minutesNumberPicker.maxValue = 59
+        minutesNumberPicker.minValue = 0
+        minutesNumberPicker.value = 0
+
+        secondsNumberPicker.maxValue = 59
+        secondsNumberPicker.minValue = 0
+        secondsNumberPicker.value = 5
+    }
+
+    private fun hideNumberPickers() {
+        hoursNumberPicker.visibility = View.GONE
+        minutesNumberPicker.visibility = View.GONE
+        secondsNumberPicker.visibility = View.GONE
+    }
+
+    private fun showNumberPickers() {
+        hoursNumberPicker.visibility = View.VISIBLE
+        minutesNumberPicker.visibility = View.VISIBLE
+        secondsNumberPicker.visibility = View.VISIBLE
     }
 }
